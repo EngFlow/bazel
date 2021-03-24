d1aa6615950b808489eb3521c88c8285c4ee2f0b
Ulf Adams <ulf@engflow.com>
2021-03-24 22:55:10 +0100

    Also mark AC downloads as 'DOWNLOADING'
    Change-Id: I39e535c6c79fb3a5c6728371206eb358239ea41c

--- src/main/java/com/google/devtools/build/lib/remote/RemoteSpawnRunner.java
+++ src/main/java/com/google/devtools/build/lib/remote/RemoteSpawnRunner.java
@@ -330,6 +330,7 @@ public class RemoteSpawnRunner implements SpawnRunner {
             acceptCachedResult = false;
           } else {
             try {
+              context.report(ProgressStatus.DOWNLOADING, getName());
               return downloadAndFinalizeSpawnResult(
                   actionKey.getDigest().getHash(),
                   cachedResult,
